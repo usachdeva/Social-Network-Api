1bca5f79c71dcea1b1bea6dd5a7dca61fd8422ce
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-07-06 18:11:04 -0400

    userSchema created

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true, trim: true },
+        email: {
+            type: String,
+            required: [true, "User email required"],
+            unique: true,
+            validate: {
+                validator: function (v) {
+                    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                        v
+                    );
+                },
+                message: (input) => `${input.value} is not a valid email!`,
+            },
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+        friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+    },
+    { toJSON: { virtuals: true }, id: false }
+);
+
+// virtual
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
