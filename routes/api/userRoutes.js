0a6656478bf19af7205485f3643dc8627a884386
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-07-10 19:24:25 -0400

    userController created

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,23 @@
 const router = require("express").Router();
 
+// importing the functions
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require(`../../controllers/userController.js`);
+
+// api/users
+// to get all the users
+// to add a user
+router.route("/").get(getUsers).post(createUser);
+
+// api/users/:userId
+// to get a single user
+// to update a user
+// to delete a user
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
 module.exports = router;
